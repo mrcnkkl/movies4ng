c8b05425cf3155045cfdc03d2e08c2ec260cb045
Marcin <mrcn.kkl@gmail.com>
2019-06-30 20:51:44 +0200

    pre depl

--- movies/settings.py
+++ movies/settings.py
@@ -99,7 +99,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [],
